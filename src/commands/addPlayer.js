4439c58b2a273ced55f42e1eef617a2e06ee2050
Mason Fox <masonfox22@gmail.com>
2021-09-06 10:01:20 -0400

    add todo in addPlayer

--- src/commands/addPlayer.js
+++ src/commands/addPlayer.js
@@ -3,10 +3,8 @@ const { db } = require("../firebase");
 const { timestamp } = require("../utilities")
 
 module.exports = async function addPlayer(msg) {
-    console.log(msg)
     let { content, author } = msg
     let arr = content.split(" ")
-    console.log(arr, arr.length)
     // ensure a name is provided
     if (arr.length !== 3) return msg.channel.send("I think you forgot a name after `add`!")
     let name = arr[2]
@@ -18,6 +16,7 @@ module.exports = async function addPlayer(msg) {
             addedBy: author.username,
             createdAt: timestamp()
         })
+        // TODO: go ahead and trigger the first insertion into the records table
         msg.channel.send(`🎉 **${name}** successfully added! I'll begin tracking them!`)
     } catch (error) {
         msg.channel.send(`I wasn't able to find a player named **${name}**`)
