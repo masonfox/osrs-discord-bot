bf015f0470d77c25d362f606fb6abed3559b95bf
Mason Fox <masonfox22@gmail.com>
2021-09-08 08:53:58 -0400

    remove creds.json, use .env
    - don't want to source creds.json, so use an env instead
    - this will work much better in Heroku

--- src/firebase.js
+++ src/firebase.js
@@ -2,10 +2,24 @@ require('dotenv').config();
 const firebase = require("firebase-admin");
 require("firebase/firestore");
 
-const serviceAccount = require("../creds.json"); // requires setting up a firebase service account for admin-sdk
+const env = process.env;
 
+/**
+ * Service-account auth for Firebase-admin SDK
+ */
 const app = firebase.initializeApp({
-  credential: firebase.credential.cert(serviceAccount)
+  credential: firebase.credential.cert({
+    "type": env.CRED_TYPE,
+    "project_id": env.CRED_PROJECT_ID,
+    "private_key_id": env.CRED_PRIVATE_KEY_ID,
+    "private_key": env.CRED_PRIVATE_KEY,
+    "client_email": env.CRED_CLIENT_EMAIL,
+    "client_id": env.CRED_CLIENT_ID,
+    "auth_uri": env.CRED_AUTH_URI,
+    "token_uri": env.CRED_TOKEN_URI,
+    "auth_provider_x509_cert_url": env.CRED_AUTH_PROVIDER_CERT_URL,
+    "client_x509_cert_url": env.CRED_CLIENT_CERT_URL
+  })
 });
 
 const db = firebase.firestore();
