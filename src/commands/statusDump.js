b1b75575919d4c0f904caedab1c79899e658ead1
Mason Fox <masonfox22@gmail.com>
2021-09-08 08:18:21 -0400

    remove console log from status dump

--- src/commands/statusDump.js
+++ src/commands/statusDump.js
@@ -4,7 +4,6 @@ module.exports = async function statusDump(channel) {
     const doc = await db.collection("guilds").doc(channel.guildId).get()
     if (doc.exists) {
         let data = doc.data()
-        console.log(data)
         let block = "```"
         let message = `OSRS Buddy status for ${data.guildName}:\n${block + "json\n"}${JSON.stringify(data, null, 2)}\n${block}` // just dump the entire block, use pretty JSON format
         channel.send(message)
