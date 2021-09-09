84dbb36617b252ab4fc4d1afb7b670b4bfbfb3f0
Mason Fox <masonfox22@gmail.com>
2021-09-09 00:02:59 -0400

    convert creds read to base64

--- src/firebase.js
+++ src/firebase.js
@@ -2,24 +2,14 @@ require('dotenv').config();
 const firebase = require("firebase-admin");
 require("firebase/firestore");
 
-const env = process.env;
+// retrieve base64 credentials from .env
+const creds = JSON.parse(Buffer.from(process.env.FIREBASE_CONFIG_BASE64, 'base64').toString('ascii'))
 
 /**
  * Service-account auth for Firebase-admin SDK
  */
 const app = firebase.initializeApp({
-  credential: firebase.credential.cert({
-    "type": env.CRED_TYPE,
-    "project_id": env.CRED_PROJECT_ID,
-    "private_key_id": env.CRED_PRIVATE_KEY_ID,
-    "private_key": env.CRED_PRIVATE_KEY.replace(/\\n/g, '\n'),
-    "client_email": env.CRED_CLIENT_EMAIL,
-    "client_id": env.CRED_CLIENT_ID,
-    "auth_uri": env.CRED_AUTH_URI,
-    "token_uri": env.CRED_TOKEN_URI,
-    "auth_provider_x509_cert_url": env.CRED_AUTH_PROVIDER_CERT_URL,
-    "client_x509_cert_url": env.CRED_CLIENT_CERT_URL
-  })
+  credential: firebase.credential.cert(creds)
 });
 
 const db = firebase.firestore();
