e0b82a5b8a8c5d324fd70c7975966356d7e4401a
Mason Fox <masonfox22@gmail.com>
2021-09-13 07:57:40 -0400

    sub and unsub messages

--- src/commands/subscription.js
+++ src/commands/subscription.js
@@ -12,12 +12,20 @@ exports.subscribe = async function subscribe(channel) {
     players: [],
     updatedAt: timestamp()
   });
+
+  // notify
+  channel.send(`👋 Thanks for setting me up! I'll watch for changes every 5 min! I'll post here in ${channel.name}! Happy leveling!`)
 };
 
 exports.unsubscribe = async function unsubscribe(channel) {
   const guild = channel.guild;
+  
+  // update the guild document - will not run next tick
   await db.collection("guilds").doc(guild.id).update({
     subscribed: false,
     updatedAt: timestamp()
   });
+
+  // notify
+  channel.send("I won't pester you anymore! If you change your mind, use `sub` to get rolling again!");
 };
