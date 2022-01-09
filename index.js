c559fb93242d9b73faa9edcfcfb3f18541e0605e
Mason Fox <masonfox22@gmail.com>
2022-01-08 21:34:49 -0500

    add app fire on boot

--- index.js
+++ index.js
@@ -27,6 +27,10 @@ client.on("ready", async () => {
 async function boot() {
   const guilds = await fetchGuilds(true)
   console.log(`${guilds.length} guilds are subscribed to updates!`)
+  // fire app logic
+  app.main()
+  // increment count
+  updateNextRun()
   // start cron on schedule
   cron.schedule(cronTime, () => {
     console.log(`The cron has run ${cronRuns} time${cronRuns > 1 ? "s" : ""}`)
