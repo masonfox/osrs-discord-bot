9dc7fb47403e5598a41e6438330fc831401265d3
Mason Fox <masonfox22@gmail.com>
2022-03-25 09:59:20 -0400

    temp logger adjustments
    - there was A LOT of noise in DD through the Heroku drains, so going direct to DD for right now
    - Will likely go away in the future through APM improvements

--- logger.js
+++ logger.js
@@ -1,20 +1,28 @@
-const winston  = require('winston');
+require('dotenv').config();
+const { createLogger, format, transports } = require('winston');
 
-const logger = winston.createLogger({
+const logger = createLogger({
   level: 'info', // default
-  format: winston.format.json(),
+  exitOnError: false,
+  format: format.json(),
   transports: [
-    new winston.transports.Console({ format: winston.format.simple() })
     // new winston.transports.File({ filename: 'error.log', level: 'error' }),
     // new winston.transports.File({ filename: 'combined.log' }),
   ],
 });
 
 // If we're not in production then log to the `console` with console format
-// if (process.env.NODE_ENV !== 'production') {
-//   logger.add(new winston.transports.Console({
-//     format: winston.format.simple(),
-//   }));
-// }
+if (process.env.NODE_ENV == 'production') {
+  logger.add(new transports.Http({
+    host: 'http-intake.logs.datadoghq.com',
+    path: `/api/v2/logs?dd-api-key=${process.env.DD_API_KEY}&ddsource=nodejs&service=app&host=heroku`,
+    ssl: true,
+    format: format.json()
+  }))
+} else {
+  logger.add(new transports.Console({
+    format: format.simple(),
+  }));
+}
 
 module.exports = logger
