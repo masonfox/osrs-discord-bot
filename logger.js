529e66cbd757d5b38aa1505f1cdac7e37ba601da
Mason Fox <masonfox22@gmail.com>
2022-03-25 09:30:06 -0400

    simple console log format
    - just for right now

--- logger.js
+++ logger.js
@@ -4,16 +4,17 @@ const logger = winston.createLogger({
   level: 'info', // default
   format: winston.format.json(),
   transports: [
-    new winston.transports.File({ filename: 'error.log', level: 'error' }),
-    new winston.transports.File({ filename: 'combined.log' }),
+    new winston.transports.Console({ format: winston.format.simple() })
+    // new winston.transports.File({ filename: 'error.log', level: 'error' }),
+    // new winston.transports.File({ filename: 'combined.log' }),
   ],
 });
 
 // If we're not in production then log to the `console` with console format
-if (process.env.NODE_ENV !== 'production') {
-  logger.add(new winston.transports.Console({
-    format: winston.format.simple(),
-  }));
-}
+// if (process.env.NODE_ENV !== 'production') {
+//   logger.add(new winston.transports.Console({
+//     format: winston.format.simple(),
+//   }));
+// }
 
 module.exports = logger
