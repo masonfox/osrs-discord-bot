4fb386f0c4a90319061e6020d5c5935549ca7566
Mason Fox <masonfox22@gmail.com>
2022-04-03 23:28:39 -0400

    transition subscription command to mongo

--- src/commands/subscription.js
+++ src/commands/subscription.js
@@ -1,16 +1,17 @@
 const { timestamp } = require("../utilities");
-const { db } = require("../firebase");
+const mongo = require("../db");
 
 exports.subscribe = async function subscribe(channel) {
   const guild = channel.guild;
+  const query = { guildId: guild.id };
 
-  // look for any existing players and maintain them
-  const record = await db.collection("guilds").doc(guild.id).get()
-  const players = (record.exists) ? record.data().players : []
-  const createdAt = (record.exists) ? record.data().createdAt : timestamp()
+  // look for any existing config and maintain
+  const record = await mongo.db.collection("guilds").findOne(query);
+  const players = record ? record.players : [];
+  const createdAt = record ? record.createdAt : new Date();
 
-  // set guild - will run next tick
-  await db.collection("guilds").doc(guild.id).set({
+  const data = {
+    _id: guild.id,
     players,
     createdAt,
     subscribed: true,
@@ -18,22 +19,36 @@ exports.subscribe = async function subscribe(channel) {
     channelName: channel.name,
     guildName: guild.name,
     guildId: guild.id,
-    updatedAt: timestamp()
-  });
+    updatedAt: new Date(),
+  };
+
+  // set guild - will run next tick
+  await mongo.db
+    .collection("guilds")
+    .updateOne(query, { $set: data }, { upsert: true });
 
   // notify
-  channel.send(`👋 Thanks for setting me up! I'll watch for changes every 2 hours! I'll post here in ${channel.name}! Happy leveling!`)
+  channel.send(
+    `👋 Thanks for setting me up! I'll watch for changes every 2 hours! I'll post here in ${channel.name}! Happy leveling!`
+  );
 };
 
 exports.unsubscribe = async function unsubscribe(channel) {
   const guild = channel.guild;
-  
+
   // update the guild document - will not run next tick
-  await db.collection("guilds").doc(guild.id).update({
-    subscribed: false,
-    updatedAt: timestamp()
-  });
+  await mongo.db.collection("guilds").updateOne(
+    { _id: guild.id },
+    {
+      $set: {
+        subscribed: false,
+        updatedAt: new Date(),
+      }
+    }
+  );
 
   // notify
-  channel.send("I won't pester you anymore! If you change your mind, use `sub` to get rolling again!");
+  channel.send(
+    "I won't pester you anymore! If you change your mind, use `sub` to get rolling again!"
+  );
 };
