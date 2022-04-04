a444db017cac8b93c74fb8f4869da562f3a27166
Mason Fox <masonfox22@gmail.com>
2022-04-04 01:04:17 -0400

    update statusDump for mongo

--- src/commands/statusDump.js
+++ src/commands/statusDump.js
@@ -1,13 +1,12 @@
-const { db } = require("../firebase");
+const mongo = require('../db');
 
 module.exports = async function statusDump(channel) {
-    const doc = await db.collection("guilds").doc(channel.guildId).get()
-    if (doc.exists) {
-        let data = doc.data()
+    const guild = await mongo.db.collection("guilds").findOne({ _id: channel.guild.id })
+    if (guild) {
         let block = "```"
-        let message = `OSRS Buddy status for ${data.guildName}:\n${block + "json\n"}${JSON.stringify(data, null, 2)}\n${block}` // just dump the entire block, use pretty JSON format
+        let message = `OSRS Buddy status for **${guild.guildName}**:\n${block + "json\n"}${JSON.stringify(guild, null, 2)}\n${block}` // just dump the entire block, use pretty JSON format
         channel.send(message)
     } else {
-        channel.send("Doesn't look like I'm currently running or have run here previously. Use the `start` command to kick me off!")
+        channel.send("I'm not configured for this server! Use the `!osrs sub` command to kick me off!")
     }
 }
