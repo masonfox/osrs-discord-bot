b9983c5c24747ae29dd2a168f47456099a2378ad
Mason Fox <masonfox22@gmail.com>
2022-04-04 01:01:00 -0400

    remove session import

--- src/commands/index.js
+++ src/commands/index.js
@@ -4,7 +4,6 @@ const { subscribe, unsubscribe } = require("./subscription")
  * Location to pull in all commands
  */
 exports.listPlayers = require("./listPlayers")
-exports.session = require("./session")
 exports.listCommands = require("./listCommands")
 exports.addPlayer = require("./addPlayer")
 exports.removePlayer = require("./removePlayer")
