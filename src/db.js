6b523d221426a5931174400fd6ad7770911a2832
Mason Fox <masonfox22@gmail.com>
2022-04-04 18:56:16 -0400

    add mongodb connection string to env

--- src/db.js
+++ src/db.js
@@ -1,14 +1,14 @@
 const { MongoClient } = require('mongodb');
 
+if(!process.env.MONGODB_CONNECTION_STRING) throw new Error("MongoDB connection string not provided in .env!")
+
 // Config
 const dbName = "osrsbuddy";
 
 // Connection
 class Mongo {
-    constructor() {
-      const url = 'mongodb://localhost:27017';
-  
-      this.client = new MongoClient(url);
+    constructor() {  
+      this.client = new MongoClient(process.env.MONGODB_CONNECTION_STRING);
     }
     async init() {
       await this.client.connect();
