aec8f898b71c045a40c37f9afc1a4bc898685979
Mason Fox <masonfox22@gmail.com>
2022-04-09 00:08:08 -0400

    add recap command to list
    - #18

--- src/commands/listCommands.js
+++ src/commands/listCommands.js
@@ -4,8 +4,9 @@ module.exports = function listCommands(msg) {
       > - \`sub\` - begin watching tracked players
       > - \`unsub\` - stop watching tracked players
       > - \`list\` - lists all of the currently tracked players
-      > - \`add "osrs name"\` - add a player to track
-      > - \`remove "osrs name"\` - remove a tracked player
+      > - \`add "rsn"\` - add a player to track
+      > - \`remove "rsn"\` - remove a tracked player
+      > - \`recap "rsn" day/week/month\` - complies the progress history over the selected timeframe
       > - \`when\` - reads out the next update time
       > - \`status\` - provide a status dump for this channel
       > - \`donate\` - info. about donating ❤️`);
