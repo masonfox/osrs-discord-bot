3ff088bfd71977f540709f9d9dde9cbf11782076
Mason Fox <masonfox22@gmail.com>
2022-04-11 09:22:01 -0400

    channel and guild mutation event handlers

---
+++ src/events/guildDelete.js
@@ -0,0 +1,11 @@
+// TODO: handle guilds where the bot is watching being deleted
+
+module.exports = {
+    name: "guildDelete",
+    once: false,
+    async execute(guild) {
+      console.log("A guild was deleted");
+      console.log(guild);
+    }
+  }
+  
