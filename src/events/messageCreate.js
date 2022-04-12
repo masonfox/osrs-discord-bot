fd6389f3994bccd2aaec5517ef8bc8e2ee3c1eb7
Mason Fox <masonfox22@gmail.com>
2022-04-11 21:54:09 -0400

    basic messageCreate event handler

---
+++ src/events/messageCreate.js
@@ -0,0 +1,21 @@
+const client = require("../client")
+
+module.exports = {
+  name: "messageCreate",
+  once: false,
+  async execute(message) {
+    //Check if author is a bot or the message was sent in dms and return
+    if (message.author.bot) return;
+    if (message.channel.type === "dm") return;
+    //get prefix from config and prepare message so it can be read as a command
+    let messageArray = message.content.split(" ");
+    let cmd = messageArray[0];
+    let args = messageArray.slice(1);
+    //Check for prefix
+    if (!cmd.startsWith("!osrs")) return;
+    //Get the command from the commands collection and then if the command is found run the command file
+    let commandfile = client.commands.get(args[0]);
+    console.log(commandfile)
+    if (commandfile) commandfile.execute(message, args);
+  },
+};
