15734911579afa5f313d122cb4081e7270fb2d7c
Mason Fox <masonfox22@gmail.com>
2022-04-11 21:32:15 -0400

    add production deploy commands

--- src/deployCommands.js
+++ src/deployCommands.js
@@ -1,29 +1,36 @@
 const client = require("./client");
-const { REST } = require('@discordjs/rest');
-const { Routes } = require('discord-api-types/v9');
+const { REST } = require("@discordjs/rest");
+const { Routes } = require("discord-api-types/v9");
+const logger = require("../logger");
 
-const rest = new REST({ version: '9' }).setToken(process.env.CLIENT_TOKEN);
+const rest = new REST({ version: "9" }).setToken(process.env.CLIENT_TOKEN);
 
 /**
  * Deploys slash commands through the Discord API
  */
 module.exports = async function deployCommands() {
   try {
-    console.log("Started refreshing application (/) commands.");
+    logger.info("Started refreshing application (/) commands.");
 
     // reduce command objects down to builder info
     const commands = client.commands.map((command) => command.data);
 
     if (process.env.NODE_ENV == "production") {
-      
+      await rest.put(Routes.applicationCommands(process.env.DISCORD_APP_ID), {
+        body: commands,
+      });
     } else {
-        await rest.put(Routes.applicationGuildCommands(process.env.DISCORD_APP_ID, process.env.TEST_DISCORD_GUILD_ID), {
-            body: commands,
-        });
+      await rest.put(
+        Routes.applicationGuildCommands(
+          process.env.DISCORD_APP_ID,
+          process.env.TEST_DISCORD_GUILD_ID
+        ),
+        { body: commands, }
+      );
     }
 
-    console.log("Successfully reloaded application (/) commands.");
+    logger.info("Successfully reloaded application (/) commands.");
   } catch (error) {
-    console.error("Error attempting to update slash commands: ", error);
+    logger.error("Error attempting to update slash commands: ", error);
   }
 };
