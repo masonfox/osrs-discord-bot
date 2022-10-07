4c9cb88502eabbc11030b493ef8afeb70bb0b6c5
Mason Fox <masonfox22@gmail.com>
2022-10-07 11:08:17 -0400

    recap command uses dropdown #27

--- src/commands/recap.js
+++ src/commands/recap.js
@@ -1,16 +1,15 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageActionRow, MessageSelectMenu } = require('discord.js');
 const { recap } = require('../app/recap');
-const { fetchGuildById } = require('../utilities');
+const { fetchGuildById, fetchGuildPlayers } = require('../utilities');
 const client = require('../client');
 const htmlToPng = require('../htmlToPng');
+
+let timeframe = '';
 
 exports.data = new SlashCommandBuilder()
   .setName('recap')
   .setDescription("View a history of a player's updates over a period of time")
-  .addStringOption((option) => option
-    .setName('rsn')
-    .setDescription('The OSRS player name')
-    .setRequired(true))
   .addStringOption((option) => option
     .setName('timeframe')
     .setDescription('How far back you want to look')
@@ -20,13 +19,8 @@ exports.data = new SlashCommandBuilder()
     .setRequired(true));
 
 exports.execute = async (interaction, logger) => {
-  await interaction.deferReply({ ephemeral: true });
-  // get options
-  const timeframe = interaction.options.getString('timeframe');
-  const rsn = interaction.options.getString('rsn');
-  const rsnLowered = rsn.toLowerCase();
-
-  logger.info('Command args', { args: { player: rsnLowered, timeframe } });
+  // get timeline and store
+  timeframe = interaction.options.getString('timeframe');
 
   // get the guild for validation
   const guild = await fetchGuildById(interaction.guildId);
@@ -37,7 +31,42 @@ exports.execute = async (interaction, logger) => {
     return interaction.editReply({ content: "Hm, this server isn't subscribed yet! Use \`/subscribe\` to get started or re-activate!" });
   }
 
-  const final = await recap([rsnLowered], timeframe);
+  const trackedPlayers = await fetchGuildPlayers(interaction.guildId);
+
+  if (trackedPlayers.length === 0) {
+    return interaction.reply({
+      content: "You're not tracking any players yet.",
+      ephemeral: true,
+    });
+  }
+
+  const playerOptions = trackedPlayers.map((player) => ({
+    label: player,
+    value: player,
+  }));
+
+  const row = new MessageActionRow().addComponents(
+    new MessageSelectMenu()
+      .setCustomId('recapPlayers')
+      .setPlaceholder('Select player')
+      .addOptions(playerOptions),
+  );
+
+  return interaction.reply({
+    content: 'Tracked player to recap:',
+    components: [row],
+    ephemeral: true,
+  });
+};
+
+exports.handler = async (interaction, handlerId, logger) => {
+  await interaction.deferUpdate();
+
+  const rsn = interaction.values[0];
+
+  logger.info('Command args', { args: { player: rsn, timeframe } });
+
+  const final = await recap([rsn], timeframe);
 
   if (final.length > 0) {
     const record = final[0];
@@ -51,12 +80,19 @@ exports.execute = async (interaction, logger) => {
       },
     });
 
+    const guild = await fetchGuildById(interaction.guildId);
     const channel = client.channels.cache.get(guild.channelId);
-    htmlToPng(channel, `Ad-hoc **${timeframe}** recap for **${rsn}**!`, [], final);
-    return interaction.editReply(`Sending recap to <#${guild.channelId}>`);
+    htmlToPng(channel, `Ad-hoc **${timeframe}** recap for **${rsn}**! Requested by ${interaction.user.username}.`, [], final);
+    return interaction.editReply({
+      content: `Sending recap to <#${guild.channelId}>`,
+      components: [],
+    });
   }
 
-  logger.info('Not enough recap data for command');
+  logger.info('Not enough recap data for player');
 
-  return interaction.editReply("We don't show any history for this player in this timeframe. Either try a different timeframe or try again later!");
+  return interaction.editReply({
+    content: "We don't show any history for this player in this timeframe. Either try a different timeframe or try again later!",
+    components: [],
+  });
 };
